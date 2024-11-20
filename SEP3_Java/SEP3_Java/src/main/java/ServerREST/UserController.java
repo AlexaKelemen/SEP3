23116447133dfcf156837b720fda8559a28f9707
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-11-20 20:43:58 +0100

    Minor change to what is returned from getUser http request (now returns ResponseEntity)

--- SEP3_Java/SEP3_Java/src/main/java/ServerREST/UserController.java
+++ SEP3_Java/SEP3_Java/src/main/java/ServerREST/UserController.java
@@ -5,6 +5,8 @@ import Database.DAOInterface.UserDAOInterface;
 import Database.Implementation.UserDAO;
 import Entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,13 +19,14 @@ public class UserController
   }
 
   @GetMapping("/users/{username}")
-  public String getUser(@PathVariable(value = "username") String username)
+  public ResponseEntity<UserDTO> getUser(@PathVariable(value = "username") String username)
   {
     User user = userDAO.getUser(username);
     if (user == null)
     {
-      return new UserDTO(new User("", "")).toJson();
+      //return new UserDTO(new User("", "")).toJson();
+      return new ResponseEntity<>(new UserDTO(), HttpStatus.NOT_FOUND);
     }
-    return new UserDTO(user).toJson();
+    return new ResponseEntity<>(new UserDTO(user), HttpStatus.OK);
   }
 }
