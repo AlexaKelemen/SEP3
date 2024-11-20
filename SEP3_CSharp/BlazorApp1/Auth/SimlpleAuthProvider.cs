d235043d474d8f70aaee9c0e214759653e13e744
BattyAlex <Alexandriaahansen@gmail.com>
2024-11-20 12:24:49 +0100

    Login partially done

---
+++ SEP3_CSharp/BlazorApp1/Auth/SimlpleAuthProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorApp1.Auth;
+using System.Security.Claims;
+using System.Text.Json;
+using DataTransferObjects;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+
+public class SimpleAuthProvider : AuthenticationStateProvider
+{
+    private readonly HttpClient httpClient;
+    private readonly IJSRuntime jsRuntime;
+
+
+    public SimpleAuthProvider(HttpClient httpClient, IJSRuntime jsRuntime)
+    {
+        this.httpClient = httpClient;
+        this.jsRuntime = jsRuntime;
+    }
+
+    public async Task Login(string userName, string password)
+    {
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+    }
+}
