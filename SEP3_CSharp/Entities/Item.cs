8e49bd52732eba4a27296c5bad36f6b83b2d8028
BattyAlex <Alexandriaahansen@gmail.com>
2024-11-20 13:25:15 +0100

    Update Item.cs

--- SEP3_CSharp/Entities/Item.cs
+++ SEP3_CSharp/Entities/Item.cs
@@ -5,6 +5,8 @@ namespace Entities;
 public class Item
 {
     public int ItemId { get; set; }
+    public string Colour {get; set;}
+    public string Size {get; set;}
     public float Price { get; set; }
     public string Description { get; set; } = null!;
     public string Name { get; set; } = null!;
