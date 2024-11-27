91d8c4e2c9a7dcf76d2b4d5660feca5ff39bdccd
BattyAlex <Alexandriaahansen@gmail.com>
2024-11-27 13:30:10 +0100

    Edit user progress

--- SEP3_CSharp/Entities/Card.cs
+++ SEP3_CSharp/Entities/Card.cs
@@ -1,12 +1,27 @@
-﻿namespace Entities;
+﻿using System.Text.RegularExpressions;
+
+namespace Entities;
 
 public class Card
 {
     public int CardId { get; set; }
-    public string CardNumber { get; set; } = null!;
+    public string CardNumber 
+    {
+        get
+        {
+            return CardNumber;
+        }
+        set
+        {
+            if (!Regex.IsMatch(value, @"^\d{16}$"))
+                throw new FormatException("Input must consist of exactly 16 digits.");
+            CardNumber = value;
+        } } 
     public DateOnly ExpirationDate { get; set; }
     public int Cvc { get; set; }
     public String FirstName { get; set; } = null!;
     public String LastName { get; set; } = null!;
     public String Username { get; set; } = null!;
+    
+    
 }
