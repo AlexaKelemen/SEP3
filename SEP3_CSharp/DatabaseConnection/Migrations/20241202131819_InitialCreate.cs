57c01289047fa0032a9a33c077223f259f6ce01d
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-12-02 14:59:39 +0100

    blazor

---
+++ SEP3_CSharp/DatabaseConnection/Migrations/20241202131819_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseConnection.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(type: "int", nullable: false).Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                },
+                constraints: table => { table.PrimaryKey("PK_Items", x => x.ItemId); }
+            );
+            migrationBuilder.InsertData(
+                table: "Items",
+                columns: new[] { "ItemId", "Name", "Description", "ImageURL", "Price", "Quantity" },
+                values: new object[,]
+                {
+                    {
+                        1, 1, "Comfy, thick base and lace, beige-coloured shoes", "/Images/shoes/shoes1", 30m, 70
+                    }
+                }
+            );
+        }
+
+    }
+    
+}
