355d18e000bc4e313345705301254420938014c8
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-12-02 10:25:52 +0100

    ItemDTO

---
+++ SEP3_CSharp/DataTransferObjects/ItemDTO.cs
@@ -0,0 +1,110 @@
+﻿namespace DataTransferObjects;
+
+public class ItemDTO
+{
+    public int itemId;
+    public String name;
+    public String description;
+    public double price;
+    public int quantity;
+    public int categoryId;
+    public String categoryName;
+    public String imageURL;
+
+    public ItemDTO()
+    {
+    }
+
+    public int getItemId()
+    {
+        return itemId;
+    }
+
+    public void setItemId(int itemId)
+    {
+        this.itemId = itemId;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public int getCategoryId()
+    {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId)
+    {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName()
+    {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName)
+    {
+        this.categoryName = categoryName;
+    }
+
+    public String getImageURL()
+    {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL)
+    {
+        this.imageURL = imageURL;
+    }
+
+    public ItemDTO(int itemId, String name, String description, double price, int quantity, int categoryId,
+        String categoryName, String imageURL)
+    {
+        this.itemId = itemId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.imageURL = imageURL;
+    }
+}
