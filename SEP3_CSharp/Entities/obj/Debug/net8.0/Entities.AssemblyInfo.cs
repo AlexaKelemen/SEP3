7b7572cd2cb1e2905ee23567f799596243978758
BattyAlex <Alexandriaahansen@gmail.com>
2024-12-02 11:15:13 +0100

    Popup in edit user

--- SEP3_CSharp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ SEP3_CSharp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1854903c997ce548d7fab9db7f9c376c2d70232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aaa0f228b6e1b787d3d215770e44a28c93d0637")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
