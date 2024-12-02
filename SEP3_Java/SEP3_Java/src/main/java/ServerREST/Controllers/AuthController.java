c079de67d0f2a74dff7fb72dbb22f3ba85cbdc5e
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-12-02 12:48:22 +0100

    Authentication implementation possibly complete

--- SEP3_Java/SEP3_Java/src/main/java/ServerREST/Controllers/AuthController.java
+++ SEP3_Java/SEP3_Java/src/main/java/ServerREST/Controllers/AuthController.java
@@ -1,8 +1,49 @@
 package ServerREST.Controllers;
 
+import DataTransferObjects.UserDTO;
+import Database.DAOInterface.UserDAOInterface;
+import Database.Implementation.UserDAO;
+import Entities.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 
 @RestController
 public class AuthController
 {
+  private UserDAOInterface userDAO;
+  public AuthController()
+  {
+    userDAO = UserDAO.getInstance();
+  }
+
+  @PostMapping("auth/login")
+  public synchronized ResponseEntity<UserDTO> login(@RequestBody User request)
+  {
+    User user;
+    try
+    {
+      user = userDAO.getUser(request.getUsername());
+    }
+    catch (Exception e)
+    {
+      System.out.println(e.getMessage());
+      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+    if(user == null)
+    {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    if(!user.getPassword().equals(request.getPassword()))
+    {
+      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+    UserDTO response = new UserDTO(user);
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
 }
