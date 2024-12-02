3cb3d752cb5f7a7710ffb2e4540243a17d526ef5
AlexaKelemen <kelemenalexa85@gmail.com>
2024-12-02 10:55:41 +0100

    item class manager created

--- SEP3_CSharp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ SEP3_CSharp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f3c55fc664229b9a298a6863c4a4a5050cf6ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1854903c997ce548d7fab9db7f9c376c2d70232")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
