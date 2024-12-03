c9fb6bf5bedf4225864eda8fb8de039d3a53912d
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-12-03 11:11:01 +0100

    This is not a hotel management system Kriszti, thank you very much

--- SEP3_Java/SEP3_Java/src/main/java/Database/DatabaseFactory.java
+++ SEP3_Java/SEP3_Java/src/main/java/Database/DatabaseFactory.java
@@ -15,7 +15,7 @@ public class DatabaseFactory
   {
     try
     {
-      return DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=hotelmanagementsystem", "postgres", "Why1sthisn33ded");
+      return DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=user_domain", "postgres", "Why1sthisn33ded");
     }
     catch (SQLException e)
     {
