0eff77795e87149cc1e4132e8546862ea0555228
AlexaKelemen <kelemenalexa85@gmail.com>
2024-12-03 15:02:21 +0100

    Cartpages attempt
    I have attempted to do a more troughout implementation of the cart in razor pages, but it needs some refinemenent and changes .I have left in the older class just in case

---
+++ SEP3_CSharp/BlazorApp1/Components/Pages/ShoppingCartBaseTry.cs
@@ -0,0 +1,58 @@
+﻿using BlazorApp1.Services.Contracts;
+using Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorApp1.Components.Pages
+
+{
+    public class ShoppingCartBaseTry : ComponentBase
+    {
+        [Inject] public IJSRuntime Js { get; set; }
+
+        [Inject] public ICartService CartService { get; set; }
+
+        private Dictionary<int, (Entities.Item Item, int Quantity)> CartItems =
+            new Dictionary<int, (Item, int)>();
+
+        private decimal CartTotal => (decimal)CartItems.Sum(entry =>
+            entry.Value.Item.Price * entry.Value.Quantity);
+
+        private void AddToCart(Item product, int quantity)
+        {
+            if (CartItems.ContainsKey(product.ItemId))
+            {
+                CartItems[product.ItemId] = (product,
+                    CartItems[product.ItemId].Quantity + quantity);
+            }
+            else
+            {
+                CartItems[product.ItemId] = (product, quantity);
+            }
+        }
+
+
+        private void UpdateQuantity(int id, int quantity)
+        {
+            if (CartItems.ContainsKey(id) && quantity > 0)
+            {
+                CartItems[id] = (CartItems[id].Item, quantity);
+            }
+        }
+
+        private void RemoveFromCart(int itemId)
+        {
+            if (CartItems.ContainsKey(itemId))
+            {
+                CartItems.Remove(itemId);
+            }
+        }
+
+
+        private void Checkout()
+        {
+            CartItems.Clear();
+            StateHasChanged();
+        }
+    }
+}
