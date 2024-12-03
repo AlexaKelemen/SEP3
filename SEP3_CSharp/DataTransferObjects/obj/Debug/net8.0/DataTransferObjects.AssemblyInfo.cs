5b234059d38ad4e00a7d9227b2f8916b59c6cb39
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-12-03 11:07:21 +0100

    Login server side fixes

--- SEP3_CSharp/DataTransferObjects/obj/Debug/net8.0/DataTransferObjects.AssemblyInfo.cs
+++ SEP3_CSharp/DataTransferObjects/obj/Debug/net8.0/DataTransferObjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataTransferObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd7ad408d1878d99b7560d733cf6250ad499403c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31faafe624ef2f80a54322facfb90207bae93398")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataTransferObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataTransferObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SEP3_CSharp/DatabaseConnection/obj/Debug/net8.0/DatabaseConnection.AssemblyInfo.cs
+++ SEP3_CSharp/DatabaseConnection/obj/Debug/net8.0/DatabaseConnection.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseConnection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd7ad408d1878d99b7560d733cf6250ad499403c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31faafe624ef2f80a54322facfb90207bae93398")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseConnection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseConnection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
