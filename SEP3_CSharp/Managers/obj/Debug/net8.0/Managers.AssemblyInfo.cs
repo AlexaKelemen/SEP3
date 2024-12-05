cf41e9ab58a4521177a86ea951366a7fb27879b1
AlexaKelemen <kelemenalexa85@gmail.com>
2024-12-05 15:25:13 +0100

    product detail page creation

--- SEP3_CSharp/Managers/obj/Debug/net8.0/Managers.AssemblyInfo.cs
+++ SEP3_CSharp/Managers/obj/Debug/net8.0/Managers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Managers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f71bf06c51bbae0c12e3c39e5efd9112191e3c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c724449f3f7ad3ff79c5b96fd908645cfba0948a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Managers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Managers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
