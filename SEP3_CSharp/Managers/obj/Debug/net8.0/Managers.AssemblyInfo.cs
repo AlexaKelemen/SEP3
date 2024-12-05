42a75cd84e6a8bfaa7d38a344c65c8a1c7e3c748
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-12-05 10:29:05 +0100

    database

--- SEP3_CSharp/Managers/obj/Debug/net8.0/Managers.AssemblyInfo.cs
+++ SEP3_CSharp/Managers/obj/Debug/net8.0/Managers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Managers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca37efa6edd4277a1faf692d40bb855c16d8f37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f90b30a537de22c4ab2068e11889511fc257573")]
 [assembly: System.Reflection.AssemblyProductAttribute("Managers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Managers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
