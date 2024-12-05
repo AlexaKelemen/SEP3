c724449f3f7ad3ff79c5b96fd908645cfba0948a
AlexaKelemen <kelemenalexa85@gmail.com>
2024-12-05 15:20:16 +0100

    attempt at product description base  and iitemservice methods

---
+++ SEP3_CSharp/DataTransferObjects/ItemCategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+using Entities.Utilities;
+
+namespace DataTransferObjects;
+
+public class ItemCategoryDTO
+{
+    public Item ItemId { get; set; }
+    public Category CategoryId { get; set; } 
+    public string CategoryName { get; set; }
+    
+}
