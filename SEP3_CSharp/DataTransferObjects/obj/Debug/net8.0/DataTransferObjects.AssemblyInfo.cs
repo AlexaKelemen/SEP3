183ce5dfa6a10cc5740113e7aa706f51ce64b3aa
AlexaKelemen <kelemenalexa85@gmail.com>
2024-12-09 09:37:05 +0100

    contact changed to about us in home page button

--- SEP3_CSharp/DataTransferObjects/obj/Debug/net8.0/DataTransferObjects.AssemblyInfo.cs
+++ SEP3_CSharp/DataTransferObjects/obj/Debug/net8.0/DataTransferObjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataTransferObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a897d198f78025a967750338acae687ddceb861d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4cebea6498bb8e0da05ddd97b8efbd0b6dddc70")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataTransferObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataTransferObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SEP3_CSharp/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ SEP3_CSharp/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a897d198f78025a967750338acae687ddceb861d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4cebea6498bb8e0da05ddd97b8efbd0b6dddc70")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
