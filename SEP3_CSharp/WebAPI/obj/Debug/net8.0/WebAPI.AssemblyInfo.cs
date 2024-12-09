d83bb2fce2ee71fc5d3405f1b938d2b43501c4e0
BattyAlex <Alexandriaahansen@gmail.com>
2024-12-09 14:41:47 +0100

    Refactoring

--- SEP3_CSharp/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ SEP3_CSharp/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24df659c0563422c9e37c3776fad9b9da470dd7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b524b641c4177ffa4526037a10a026cf9ee9fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
