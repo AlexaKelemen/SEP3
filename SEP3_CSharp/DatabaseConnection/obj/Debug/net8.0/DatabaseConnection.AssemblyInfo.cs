4454c2204bb7cb88638f7a7a49497b22edf36b8a
BattyAlex <Alexandriaahansen@gmail.com>
2024-12-09 14:03:43 +0100

    Button fix

--- SEP3_CSharp/DatabaseConnection/obj/Debug/net8.0/DatabaseConnection.AssemblyInfo.cs
+++ SEP3_CSharp/DatabaseConnection/obj/Debug/net8.0/DatabaseConnection.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseConnection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba93cd551bb6d1538173ecbd898fb038162be92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24df659c0563422c9e37c3776fad9b9da470dd7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseConnection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseConnection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
