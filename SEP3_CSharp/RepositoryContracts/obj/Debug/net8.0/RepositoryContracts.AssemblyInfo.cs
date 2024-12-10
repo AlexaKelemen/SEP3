8529e3bf501276131045f5aa930d4ee10d1a3364
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-12-10 15:44:09 +0100

    the quantity is updated in the shopping cart

--- SEP3_CSharp/RepositoryContracts/obj/Debug/net8.0/RepositoryContracts.AssemblyInfo.cs
+++ SEP3_CSharp/RepositoryContracts/obj/Debug/net8.0/RepositoryContracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4e9cc25bc6f0fb2003847f2f58d4302ab61fef9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404e8903e921124c237fd1cfe6ed791d69bb2724")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
