c4e9cc25bc6f0fb2003847f2f58d4302ab61fef9
AlexaKelemen <kelemenalexa85@gmail.com>
2024-12-10 12:48:27 +0100

    database  change
    addedd more clothes, changed prices and added categories to items

--- SEP3_CSharp/DatabaseConnection/AppDbContext.cs
+++ SEP3_CSharp/DatabaseConnection/AppDbContext.cs
@@ -10,6 +10,7 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+
     public DbSet<Item> Items => Set<Item>();
     public DbSet<Category> Categories => Set<Category>();
     public DbSet<Order> Orders => Set<Order>();
@@ -25,7 +26,7 @@ public class AppDbContext : DbContext
             Name = "Comfy shoes",
             Description = "Tired of painful soles? Try this now!",
             ImageURL = "Images/Shoes/shoes1.png",
-            Price = 10,
+            Price = 270,
             Quantity = 100,
             Colour = "Blue",
             Size = "Small",
@@ -37,7 +38,7 @@ public class AppDbContext : DbContext
             Name = "Running shoes",
             Description = "Perfect fit for a runner",
             ImageURL = "Images/Shoes/shoes2.png",
-            Price = 5,
+            Price = 675,
             Quantity = 100,
             Colour = "Red",
             Size = "Small",
@@ -49,7 +50,7 @@ public class AppDbContext : DbContext
             Name = "Summer dress",
             Description = " Comfortable, long and flowy!",
             ImageURL = "Images/Clothes/clothing1.jpg",
-            Price = 15,
+            Price = 540,
             Quantity = 100,
             Colour = "Green",
             Size = "Large",
@@ -59,9 +60,10 @@ public class AppDbContext : DbContext
         {
             ItemId = 4,
             Name = "White Sneakers",
-            Description = "Classic white lace up sneakers with a minimalist design",
+            Description =
+                "Classic white lace up sneakers with a minimalist design",
             ImageURL = "Images/Shoes/shoes4.png",
-            Price = 10,
+            Price = 1100,
             Quantity = 100,
             Colour = "Grey",
             Size = "Small",
@@ -73,7 +75,7 @@ public class AppDbContext : DbContext
             Name = "White and black ankle sneakers",
             Description = "Beautiful blend from white to black",
             ImageURL = "Images/Shoes/shoes5.png",
-            Price = 20,
+            Price = 710,
             Quantity = 100,
             Colour = "Red",
             Size = "Small",
@@ -85,7 +87,7 @@ public class AppDbContext : DbContext
             Name = "Amazing footmat",
             Description = "Perfect for driving people away from your house",
             ImageURL = "Images/accessories/Accessory21.png",
-            Price = 30,
+            Price = 250,
             Quantity = 100,
             Colour = "Brown",
             Size = "Small",
@@ -97,7 +99,7 @@ public class AppDbContext : DbContext
             Name = "Eyeglasses",
             Description = "Stylish glasses with black, round frames",
             ImageURL = "Images/accessories/Accessory7.png",
-            Price = 10,
+            Price = 1000,
             Quantity = 100,
             Colour = "Black",
             Size = "Small",
@@ -109,7 +111,7 @@ public class AppDbContext : DbContext
             Name = "Small Hand bag",
             Description = "Brown hand bag with reinforced handles",
             ImageURL = "Images/accessories/Accessory11.png",
-            Price = 27,
+            Price = 800,
             Quantity = 100,
             Colour = "Brown",
             Size = "Small",
@@ -121,7 +123,7 @@ public class AppDbContext : DbContext
             Name = "Funny ducks",
             Description = "Perfect gift to give to lovers of ducks!",
             ImageURL = "Images/accessories/Accessory14.png",
-            Price = 7,
+            Price = 75,
             Quantity = 100,
             Colour = "White",
             Size = "Small",
@@ -133,7 +135,7 @@ public class AppDbContext : DbContext
             Name = "Funny clothespin",
             Description = "Good, durable quality",
             ImageURL = "Images/accessories/Accessory19.png",
-            Price = 5,
+            Price = 55,
             Quantity = 100,
             Colour = "Black & White",
             Size = "Small",
@@ -145,7 +147,7 @@ public class AppDbContext : DbContext
             Name = "Funny sticker",
             Description = "Durable but easily removed glue",
             ImageURL = "Images/accessories/Accessory17.png",
-            Price = 5,
+            Price = 50,
             Quantity = 100,
             Colour = "Brown",
             Size = "Small",
@@ -157,7 +159,7 @@ public class AppDbContext : DbContext
             Name = "Small, feminine wallet",
             Description = "Beautiful leather wallet",
             ImageURL = "Images/accessories/Accessory10.png",
-            Price = 15,
+            Price = 530,
             Quantity = 100,
             Colour = "Black & Brown",
             Size = "Small",
@@ -169,7 +171,7 @@ public class AppDbContext : DbContext
             Name = "Bright green dress",
             Description = "Comfortable cotton. One out of 10 girls recommend",
             ImageURL = "Images/Clothes/clothing2.jpg",
-            Price = 25,
+            Price = 250,
             Quantity = 100,
             Colour = "Green",
             Size = "Small",
@@ -181,7 +183,7 @@ public class AppDbContext : DbContext
             Name = "Beautiful black dinner dress",
             Description = "Very comfortable and stretchy material",
             ImageURL = "Images/Clothes/clothing3.jpg",
-            Price = 35,
+            Price = 2500,
             Quantity = 100,
             Colour = "Black",
             Size = "Small",
@@ -193,7 +195,7 @@ public class AppDbContext : DbContext
             Name = "Black ankle boots",
             Description = "Sleek black boots with low heels and a zipper",
             ImageURL = "Images/shoes/shoes16.png",
-            Price = 12,
+            Price = 1200,
             Quantity = 100,
             Colour = "Black",
             Size = "Small",
@@ -205,17 +207,208 @@ public class AppDbContext : DbContext
             Name = "Black bracelet",
             Description = "Durable, stretchy bracelet",
             ImageURL = "Images/accessories/Accessory6.png",
-            Price = 5,
+            Price = 150,
             Quantity = 100,
             Colour = "Black",
             Size = "Small",
         });
+        modelBuilder.Entity<Item>().ToTable("Items");
+        modelBuilder.Entity<Item>().HasData(new Item
+        {
+            ItemId = 17,
+            Name = "The flow dress",
+            Description = "The manifestation of elegance",
+            ImageURL = "Images/Clothes/clothing4.jpg",
+            Price = 500,
+            Quantity = 100,
+            Colour = "White",
+            Size = "Small",
+        });
+        modelBuilder.Entity<Item>().ToTable("Items");
+        modelBuilder.Entity<Item>().HasData(new Item
+        {
+            ItemId = 18,
+            Name = "The dress",
+            Description = "Unleash your inner diva",
+            ImageURL = "Images/Clothes/clothing5.jpg",
+            Price = 700,
+            Quantity = 100,
+            Colour = "Red",
+            Size = "Small",
+        });
+        modelBuilder.Entity<Item>().ToTable("Items");
+        modelBuilder.Entity<Item>().HasData(new Item
+        {
+            ItemId = 19,
+            Name = "Ultimate slay",
+            Description = "Make the billion dollar deal in this dress",
+            ImageURL = "Images/Clothes/clothing6.jpg",
+            Price = 450,
+            Quantity = 100,
+            Colour = "Burgundy",
+            Size = "Small",
+        });
+        modelBuilder.Entity<Item>().ToTable("Items");
+        modelBuilder.Entity<Item>().HasData(new Item
+        {
+            ItemId = 20,
+            Name = "Dinner served",
+            Description = "Be the light of the dinner party",
+            ImageURL = "Images/Clothes/clothing7.jpg",
+            Price = 625,
+            Quantity = 100,
+            Colour = "Red",
+            Size = "Small",
+        });
+        modelBuilder.Entity<Item>().ToTable("Items");
+        modelBuilder.Entity<Item>().HasData(new Item
+        {
+            ItemId = 21,
+            Name = "Shine bright dress",
+            Description = "Make an impression,shine in your light",
+            ImageURL = "Images/Clothes/clothing8.jpg",
+            Price = 350,
+            Quantity = 100,
+            Colour = "White",
+            Size = "Small",
+        });
+
+
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 2,
+            ItemId = 1,
+        });
+
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 2,
+            ItemId = 2,
+        });
+
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 1,
+            ItemId = 3,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 2,
+            ItemId = 4,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 2,
+            ItemId = 5,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 3,
+            ItemId = 6,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 3,
+            ItemId = 7,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 3,
+            ItemId = 8,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 3,
+            ItemId = 9,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 3,
+            ItemId = 10,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 3,
+            ItemId = 11,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 3,
+            ItemId = 12,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 1,
+            ItemId = 13,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 1,
+            ItemId = 14,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 2,
+            ItemId = 15,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 3,
+            ItemId = 16,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 1,
+            ItemId = 17,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 1,
+            ItemId = 18,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 1,
+            ItemId = 19,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 1,
+            ItemId = 20,
+        });
+        modelBuilder.Entity<ItemCategory>().ToTable("CategoryItem");
+        modelBuilder.Entity<ItemCategory>().HasData(new ItemCategory()
+        {
+            CategoryId = 1,
+            ItemId = 21,
+        });
+
         modelBuilder.Entity<Category>().ToTable("Categories");
         modelBuilder.Entity<Category>().HasData(new Category
         {
-           CategoryId = 1,
-           CategoryName = "Clothing",
-           CategoryDescription = "Different kinds of clothing"
+            CategoryId = 1,
+            CategoryName = "Clothing",
+            CategoryDescription = "Different kinds of clothing"
         });
         modelBuilder.Entity<Category>().ToTable("Categories");
         modelBuilder.Entity<Category>().HasData(new Category
@@ -232,5 +425,4 @@ public class AppDbContext : DbContext
             CategoryDescription = "Different kinds of accessories"
         });
     }
-    
 }
--- SEP3_CSharp/Entities/Utilities/ItemCategory.cs
+++ SEP3_CSharp/Entities/Utilities/ItemCategory.cs
@@ -2,6 +2,8 @@
 
 public class ItemCategory
 {
-    public Item ItemId { get; set; } = null!;
-    public Category CategoryId { get; set; } = null!;
+    public int ItemId { get; set; }
+    public Item Item { get; set; } = null!;
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
 }
