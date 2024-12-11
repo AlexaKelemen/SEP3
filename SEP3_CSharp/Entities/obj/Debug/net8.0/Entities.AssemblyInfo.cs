bab8c58a01386130ebef9c5152cc93a4d91ee97e
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-12-11 12:21:55 +0100

    DisplayREturns

--- SEP3_CSharp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ SEP3_CSharp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc820f93c350b6d08eca0efde33c118568a11ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c8d200e70aeb82f0a0e2b975b44c14f2f05c3b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
