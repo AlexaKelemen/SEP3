726e11b47d2eec6d11ba078909b3c03d1e3cd4b9
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-12-12 12:48:14 +0100

    better

--- SEP3_CSharp/Managers/obj/Debug/net8.0/Managers.AssemblyInfo.cs
+++ SEP3_CSharp/Managers/obj/Debug/net8.0/Managers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Managers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e79331f73219c76eee269d0131235b8cf3881d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874adba0b81494123693d5a767b78b4d78ef4ba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Managers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Managers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SEP3_CSharp/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ SEP3_CSharp/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e79331f73219c76eee269d0131235b8cf3881d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874adba0b81494123693d5a767b78b4d78ef4ba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
