04054d04dde88805325880faa33e3d25351436a9
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-12-12 14:22:49 +0100

    why

--- SEP3_CSharp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ SEP3_CSharp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c346dec2e2ec91ded444c5795ed81053f68720")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b629952b76bb091d22614713bec1d2e3e2a72335")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
