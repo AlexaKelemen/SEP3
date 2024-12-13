45a70c88ec0e2bc01b4d6ae6418a5d2bdb1d22ea
CodeAFox <illeskrisztinacsilla@gmail.com>
2024-12-13 09:17:02 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ SEP3_Java/SEP3_Java/src/main/java/Shared/Database/DatabaseFactory.java
@@ -0,0 +1,23 @@
+package Shared.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseFactory
+{
+
+  public Connection establishConnection()
+  {
+    try
+    {
+      return DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=user_domain", "postgres", "fatcat");
+        }
+    catch (SQLException e)
+    {
+      System.out.println("Connection failed. Try a different password perhaps.");
+      return null;
+    }
+  }
+
+}
