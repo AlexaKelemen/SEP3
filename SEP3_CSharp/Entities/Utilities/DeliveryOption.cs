ce9e57fdcd3da32481429cf25150368e95ebe3e1
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2024-12-16 10:44:31 +0100

    delivery spped

--- SEP3_CSharp/Entities/Utilities/DeliveryOption.cs
+++ SEP3_CSharp/Entities/Utilities/DeliveryOption.cs
@@ -5,4 +5,5 @@ public class DeliveryOption
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string ToAddress { get; set; }
+    public string Speed { get; set; }
 }
