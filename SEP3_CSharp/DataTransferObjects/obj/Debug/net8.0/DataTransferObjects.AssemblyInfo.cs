7c8a6674a76540f25b11dc991ba6619b6e17fbc5
SophiaJustin <150365571+SophiaJustin@users.noreply.github.com>
2024-12-16 14:15:03 +0100

    changed the font

--- SEP3_CSharp/DataTransferObjects/obj/Debug/net8.0/DataTransferObjects.AssemblyInfo.cs
+++ SEP3_CSharp/DataTransferObjects/obj/Debug/net8.0/DataTransferObjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataTransferObjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b09d7322ccc335aa21c251987b4ac3bcf371df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6774ee02879e5e9de193d82c0efb8502948528")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataTransferObjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataTransferObjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
