c95e6917e1366c0141a29d4962c280a6c4406f9e
BattyAlex <Alexandriaahansen@gmail.com>
2024-12-17 16:11:44 +0100

    Idk what's happening

--- SEP3_CSharp/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ SEP3_CSharp/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad949afaf9134e87d23bfa28026807d6afa66b47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1394e62599ac6850124605b52a20a75f9413d33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
